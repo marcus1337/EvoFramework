cc48f34f3b996c606f7a0e8956571827ee224215
marcus1337 <blurgas@hotmail.com>
2022-02-19 22:17:38 +0100

    rand values

--- src/Mutation/Mutater.cpp
+++ src/Mutation/Mutater.cpp
@@ -1,2 +1,22 @@
 #include "Mutater.h"
 
+Mutater::Mutater() : randEngine(randDevice()) {
+    setWeights();
+}
+
+void Mutater::setWeights()
+{
+    const int weightsRange = 1000;
+    weightsDistribution = std::uniform_int_distribution<int>(0, weightsRange*2);
+    for (int i = weightsRange; i > 0; i--) {
+        weights.push_back((float)i / weightsRange);
+    }
+    weights.push_back(0);
+    for (int i = 1; i <= weightsRange; i++) {
+        weights.push_back(((float)i / weightsRange)*(-1.f));
+    }    
+}
+
+float Mutater::getRandomWeight() {
+    return weights[weightsDistribution(randEngine)];
+}
--- src/Mutation/Mutater.h
+++ src/Mutation/Mutater.h
@@ -1,12 +1,22 @@
 #include "NN/NN.h"
+#include <random>
 
 #ifndef MUTATER_H
 #define MUTATER_H
 
 class Mutater {
 
-public:
+    std::random_device randDevice;
+    std::mt19937 randEngine;
+    std::uniform_int_distribution<int> weightsDistribution;
+    std::vector<float> weights;
 
+    void setWeights();
+
+public:
+    float getRandomWeight();
+
+    Mutater();
 
 };
 
