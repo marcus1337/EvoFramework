2593ee351e998da9b6991977cb4607ce400e8a94
marcus1337 <blurgas@hotmail.com>
2022-02-19 18:12:00 +0100

    net unit test

--- test/tests.cpp
+++ test/tests.cpp
@@ -4,7 +4,12 @@
 #include <vector>
 #include "NN/NN.h"
 
-TEST_CASE("simple test", "[hello]") {
-    REQUIRE(true == true);
+TEST_CASE("edge count", "[nn]") {
+    NN nn({ 2, 2 });
+    REQUIRE(nn.getNumEdges() == 4);
+    nn = NN({ 2, 3, 2 });
+    REQUIRE(nn.getNumEdges() == 12);
+    nn = NN({ 2, 3, 3, 2 });
+    REQUIRE(nn.getNumEdges() == 21);
 }
 
