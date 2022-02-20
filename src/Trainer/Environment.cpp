80ebb81615f69b87e8485c8c8bb046399a1d21d9
marcus1337 <blurgas@hotmail.com>
2022-02-20 16:21:42 +0100

    env class

---
+++ src/Trainer/Environment.cpp
@@ -0,0 +1,5 @@
+#include "Environment.h"
+
+Environment::Environment(std::string _luaScript) : luaScript(_luaScript) {
+
+}
---
+++ src/Trainer/Environment.h
@@ -0,0 +1,17 @@
+#include <string>
+
+#ifndef ENVIRONMENT_H
+#define ENVIRONMENT_H
+
+class Environment {
+
+    std::string luaScript;
+
+public:
+
+    Environment(std::string _luaScript);
+
+};
+
+
+#endif // !ENVIRONMENT_H
