68f4118c1074c3e28cbe4ca498e5968e1584b3aa
marcus1337 <blurgas@hotmail.com>
2022-03-06 13:05:55 +0100

    help command

--- src/Server/HelpRequest.cpp
+++ src/Server/HelpRequest.cpp
@@ -7,10 +7,10 @@ HelpRequest::HelpRequest(std::vector<std::string> _args) : RequestCommand(_args)
 
 std::string HelpRequest::execute() {
 
-    return "API for remote training of neural networks via genetic algorithms.\n\
+    return "API for remote training of neural networks (NN) by using genetic algorithms (GA).\n\
 Possible commands:\n\
-help - Returns generic info about the API.\n\
-start script:<lua script> - Starts training neural networks with the provided lua script. Returns \'Ok\', or an error message.\n\
-stop - Stops training and returns a string representation of a neural network for the best generated solution, or an error message.\n\
+help - Returns info about the API.\n\
+start <number of AIs> script:<lua script> - Starts training NNs with the provided Lua script. <number of AIs> must be a positive integer that defines how many AIs can be tested in parallel by the GAs. Returns \'Ok\', or an error message.\n\
+stop - Stops training and returns a string representation of the best generated NN, or an error message.\n\
 stats - Info about the training progress.";
 }
