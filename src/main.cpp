2bd935c3627dd6d461039b97fe7b924d78a40e90
marcus1337 <blurgas@hotmail.com>
2022-03-09 18:03:56 +0100

    remove unused code

--- src/main.cpp
+++ src/main.cpp
@@ -1,30 +1,7 @@
-#include <iostream>
-#include "NN/NN.h"
-#include "NNLayer.h"
-#include "Mutation/Mutater.h"
-#include "Trainer/Trainer.h"
-#include "Trainer/Environment.h"
-#include "Trainer/Model.h"
-#include "Trainer/ModelVerifier.h"
 #include "Server/Server.h"
 
-void testEnv() {
-    Trainer trainer(100, ModelVerifier::getExampleModel());
-    bool startedTraining = trainer.train();
-    std::cout << "started training? " << startedTraining << "\n";
-    std::cin.get();
-    std::cout << "RESULT...\n";
-    trainer.stopTraining();
-    std::cout << trainer.getBestNN().getStringRepresentation() << "\n\n";
-}
-
-void testServer() {
+int main(int argc, char* argv[]) {
     Server server;
     server.start();
-}
-
-int main(int argc, char* argv[]) {
-    testServer();
-    //testEnv();
     return 0;
 }
